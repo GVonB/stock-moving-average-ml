ead9adfc9075c7349ddb73b79239e719abcb35d8
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-01-29 15:51:41 -0500

    Calculate moving averages

--- prediction.py
+++ prediction.py
@@ -8,8 +8,16 @@ import numpy as np
 def get_stock_data(ticker, days=150):
     return yf.download(ticker, period=f'{days}d')
 
+# Calculates moving average
+def calculate_moving_averages(data, window):
+    return data['Close'].rolling(window=window).mean()
+
 # Test case
 ticker = "AAPL"
 stock_data = get_stock_data(ticker)
+
+# Calculate moving averages
+stock_data['150_MA'] = calculate_moving_averages(stock_data, 150)
+stock_data['50_MA'] = calculate_moving_averages(stock_data, 50)
 
 print(stock_data)
